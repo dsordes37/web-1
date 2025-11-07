33f5c7903b3840055f9dc5a3e4520124c31668e8
dsordes37 <dsordesdinho342@gmail.com>
2025-11-07 00:54:03 -0300

    quickfix

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+//chamando os elementos dom
+const letter = document.getElementById("letreiro");
+const btChute = document.getElementById("chute");
+
+const input1 = document.getElementById("player1Input");
+const input2 = document.getElementById("player2Input");
+
+const hist1 = document.getElementById("hist1");
+const hist2 = document.getElementById("hist2");
+
+let onPlay=false
+
+
+
+//função que sorteia o numero
 function sortearNumero(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
@@ -10,21 +25,40 @@ function createRegister(reg){
 }
 
 
-const letter = document.getElementById("letreiro");
-const btChute = document.getElementById("chute");
+//função que inincia a partida
+function partida(){
+    input1.disabled=false;
+    input2.disabled=false;
 
-const input1 = document.getElementById("player1Input");
-const input2 = document.getElementById("player2Input");
+    btChute.innerText="Chutar";
+    onPlay=true;
 
-const hist1 = document.getElementById("hist1");
-const hist2 = document.getElementById("hist2");
+    letter.innerText="Tentem adivinhar"
+    hist1.replaceChildren();
+    hist2.replaceChildren();
+}
+
+//função que reseta o jogo
+function inicio(){
+    input1.disabled=true;
+    input1.value='';
+    input2.disabled=true;
+    input2.value='';
+
+    btChute.innerText="Iniciar nova partida";
+    onPlay=false;
+
+    
+}
 
 
-let numero = sortearNumero(1, 100);
+//funcção que verifica o resultado
+function verifica(){
 
+    if(!onPlay){
+        partida();
 
-btChute.addEventListener("click", ()=>{
-    if(input1.value!='' && input2.value!='' && input1.value>0 && input1.value<101 && input2.value>0 && input2.value<101){
+    }else if(input1.value!='' && input2.value!='' && input1.value>0 && input1.value<101 && input2.value>0 && input2.value<101){
         letter.innerText=`${input1.value} e ${input2.value}`;
 
         if(input1.value>numero){
@@ -42,19 +76,45 @@ btChute.addEventListener("click", ()=>{
 
         if(input1.value==numero && input2.value != numero){
             hist1.appendChild(createRegister(`O número é ${numero}, vitória do jogador 1`));
-            hist2.appendChild(createRegister(`O número é ${numero}, vitória do jogador 1`))
+            hist2.appendChild(createRegister(`O número é ${numero}, vitória do jogador 1`));
+            letter.innerText=`O número é ${numero}, vitória do jogador 1`;
+            inicio()
         }else if(input2.value==numero && input1.value != numero){
             hist1.appendChild(createRegister(`O número é ${numero}, vitória do jogador 2`));
             hist2.appendChild(createRegister(`O número é ${numero}, vitória do jogador 2`));
-        }else if(input2.value === numero && input1.value === numero){
+            letter.innerText=`O número é ${numero}, vitória do jogador 2`;
+            inicio()
+        }else if(input2.value == input1.value && input1.value == numero){
             hist1.appendChild(createRegister(`O número é ${numero}, empate`));
             hist2.appendChild(createRegister(`O número é ${numero}, empate`));
+            letter.innerText=`O número é ${numero}, empate`;
+            inicio()
         }
+
+        hist1.scrollTop = hist1.scrollHeight;
+        hist2.scrollTop = hist1.scrollHeight;
 
 
 
 
     }else{
         letter.innerText=`Os dois campos devem conter números entre 1 e 100`;
+    }
+}
+
+
+
+let numero = sortearNumero(1, 100);
+
+inicio();
+
+
+
+//eventlisteners
+btChute.addEventListener("click", verifica)
+
+document.addEventListener("keydown", (key)=>{
+    if(key.key=="Enter"){
+        verifica()
     }
 })
